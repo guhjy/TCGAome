0fd1045616e504cf20d72ace95b6cd14a905d71e
priesgo <priesgoferreiro@gmail.com>
2016-08-02 21:30:25 -0500

    biomaRt static variable implemented with attr

--- R/biomart.r
+++ R/biomart.r
@@ -1,24 +1,21 @@
-#library(biomaRt)
-
-# Loads the Ensembl Biomart for Homo sapiens
-load_biomart <- function ()
-{
-  if (! exists("ensembl"))
-  {
-    # Loads ensembl biomart for Homo sapiens
-    #ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
-    ensembl <<- useMart("ENSEMBL_MART_ENSEMBL", host="www.ensembl.org")
-    dataset="hsapiens_gene_ensembl"
-    ensembl <<- useDataset(dataset, mart=ensembl)
-  }
-
-  ensembl
+## Loads the Ensembl Biomart for Homo sapiens
+load_biomart <- function() {
+    ensembl <- attr(load_biomart, "cached_ensembl")
+    if (is.null(ensembl)) {
+        ## Loads ensembl biomart for Homo sapiens ensembl = useMart('ensembl',dataset='hsapiens_gene_ensembl')
+        futile.logger::flog.info("Loading biomart for the first time")
+        ensembl_mart <- biomaRt::useMart("ENSEMBL_MART_ENSEMBL", host = "www.ensembl.org")
+        dataset <- "hsapiens_gene_ensembl"
+        ensembl <- biomaRt::useDataset(dataset, mart = ensembl_mart)
+        attr(load_biomart, "cached_ensembl") <<- ensembl
+    } else {
+      futile.logger::flog.info("Returning cached biomart")
+    }
+    ensembl
 }
 
-# Queries the biomart
-query_biomart <- function (attributes, filters, values)
-{
-  # Example mapping HUGO gene names to Entrez gene IDs using BioMart
-  #map_entrez_hgnc = getBM(attributes=c("entrezgene","hgnc_symbol"), filters=c("hgnc_symbol"), values=goa$Gene, mart=ensembl)
-  getBM(attributes=attributes, filters=filters, values=values, mart=load_biomart())
+## Queries the biomart
+query_biomart <- function(attributes, filters, values) {
+    ## Example mapping HUGO gene names to Entrez gene IDs using BioMart
+    biomaRt::getBM(attributes = attributes, filters = filters, values = values, mart = load_biomart())
 }
